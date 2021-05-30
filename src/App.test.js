8d867aacb0be7a4549e734def30c03ca2aabb215
Manuel <manuelespv@gmail.com>
2021-05-30 19:25:35 -0400

    fixing homepage path and tailwind style

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
-describe('Testing App component', () => {
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-    test('First test', () => {
-
-    });
-
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
