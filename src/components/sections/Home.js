30e1476e1a37ad0c1f9ab5a2d9850226e9fc73de
Manuel <manuelespv@gmail.com>
2021-05-30 20:21:51 -0400

    adding animations

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -8,7 +8,8 @@ export const Home = () => {
     return (
         <div className="bg-gradient-to-tr from-primary to-secondary
         w-full h-screen">
-            <div className="w-full h-1/5 flex justify-center items-end">
+            <div className="w-full h-1/5 flex justify-center items-end
+            animate__animated animate__fadeInDown">
                 {
                     images.map(({id, name, src}) => (
                         <img key={id} alt={name} src={src} className="h-16 w-14 mx-5"/>
@@ -23,15 +24,15 @@ export const Home = () => {
             </div>
             <div className="w-full h-3/5 flex justify-center relative">
                 <div className="text-center text-white text-xl opacity-80 absolute right-2/3
-                animate__animated animate__fadeInBottomLeft">
+                animate__animated animate__fadeInBottomLeft z-50">
                     Ingeniero Civil Informático
                 </div>
                 <div className="text-center text-white text-xl opacity-80 absolute left-2/3
-                animate__animated animate__fadeInBottomRight">
+                animate__animated animate__fadeInBottomRight z-50">
                     Desarrollador web Fullstack
                 </div>
                 <img src="./setup/room.png" alt="Room" 
-                className="h-full absolute animate__animated animate__fadeIn"/>
+                className="h-full absolute animate__animated animate__fadeInUp z-30"/>
             </div>
         </div>
     )
